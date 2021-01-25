6a1548ef225dc2fedb78bda60b146a4b67d0afcc
akgngr <abdulkadirgngr@hotmail.com>
2021-01-25 23:46:51 +0300

    adres telefon ve diğer değişimler

--- components/homepage/slideshow.vue
+++ components/homepage/slideshow.vue
@@ -1,7 +1,7 @@
 <i18n>
 {
   "tr": {
-    "slider": "images-slider/desktop/invisalign.png",
+    "slider": "images-slider/desktop/invisalign.webp",
     "slider2": "images-slider/desktop/slider2.webp",
     "slider3": "images-slider/desktop/slider3.webp",
     "slider4": "images-slider/desktop/slider4.webp",
